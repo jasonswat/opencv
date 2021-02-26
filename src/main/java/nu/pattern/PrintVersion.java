e40356a7eb498c57642ed45b5779a4715d71f0a4
Jason von Nieda <jason@vonnieda.org>
2021-02-26 02:39:16 -0600

    no message

--- src/main/java/nu/pattern/PrintVersion.java
+++ src/main/java/nu/pattern/PrintVersion.java
@@ -7,7 +7,7 @@ public class PrintVersion {
         OpenCV.loadLocally();
     }
     
-    static void main(String[] args) {
+    public static void main(String[] args) {
         System.out.println(Core.getVersionString());
     }
 }
